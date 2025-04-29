6eb8242f3d309cab5c7123f91fec63d8c7324d98
Varun0737 <shyamala002@gannon.edu>
2025-04-29 14:15:37 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// app.js
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js App!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
